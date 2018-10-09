607495da05719854f2c571e2e5881394f8a15f5a
7KitsandKats7 <sophiebwh@gmail.com>
2018-10-09 18:16:46 -0500

    TeleOP
    Ughhghg

--- TrobotixTeleOP.java
+++ TrobotixTeleOP.java
@@ -33,6 +33,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -50,7 +52,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@TeleOp(name="TroTeleOP", group="Linear Opmode")
 @Disabled
 public class TrobotixTeleOP extends LinearOpMode {
 
@@ -58,6 +60,9 @@ public class TrobotixTeleOP extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
+    private DcMotor extender = null;
+    private Servo flipper = null;
+    private DcMotor chain = null;
 
     @Override
     public void runOpMode() {
@@ -69,11 +74,16 @@ public class TrobotixTeleOP extends LinearOpMode {
         // step (using the FTC Robot Controller app on the phone).
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        extender  = hardwareMap.get(DcMotor.class, "extender");
+        flipper = hardwareMap.get(Servo.class, "flipper");
+        chain  = hardwareMap.get(DcMotor.class, "chain");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        chain.setDirection(DcMotor.Direction.FORWARD);
+        extender.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -83,23 +93,24 @@ public class TrobotixTeleOP extends LinearOpMode {
         while (opModeIsActive()) {
 
             // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
+            double leftPower = 0;
+            double rightPower = 0;
 
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            if (gamepad2.x) chain.setPower(0.95);
+            else chain.setPower(0);
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
+            if (gamepad2.left_bumper) extender.setPower(0.5);
+            else if (gamepad2.right_bumper) extender.setPower(-0.5);
+            else extender.setPower(0);
+
+            if (gamepad2.a) flipper.setPosition(90);
+            else if (gamepad2.b) flipper.setPosition(0);
+
+            leftPower  = -gamepad1.left_stick_y;
+            rightPower = -gamepad1.right_stick_y;
 
             // Send calculated power to wheels
             leftDrive.setPower(leftPower);
